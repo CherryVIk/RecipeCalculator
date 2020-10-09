bd43a19069e415b9228232af63426993ed4a8a87
Victoria Boichenko <vik.boichenko@gmail.com>
2020-10-09 21:04:16 +0300

    Create simple UI in storyboard. Add file for constants. Add first table controller class.

---
+++ RecipeCalculator/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RecipeCalculator
+//
+//  Created by Victoria Boichenko on 09.10.2020.
+//
+
+import Foundation
+struct K{
+    static let recipeID = "RecipeID"
+    static let itemID = "ItemID"
+}
